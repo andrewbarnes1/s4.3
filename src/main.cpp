969582707d025cb22f589d2315a5eac1b222b6f4
andrew <andrewbarneswork@outlook.com>
2018-02-08 11:25:32 +0000

    Added different LCD methods to display on the screen.

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,22 @@ C12832 lcd(D11, D13, D12, D7, D10);
 int main()
 {
     int j=0;
+    // Method to clear the screen.
     lcd.cls();
+    // Set cursor at given position. (0,0) = Top left corner
     lcd.locate(0,0);
-    lcd.printf("mbed application shield!");
+    lcd.printf("Hello Andrew!");
     lcd.locate(0,10);
     lcd.printf("char %dx%d : %dx%d pixels",
                lcd.columns(), lcd.rows(),
                lcd.width(), lcd.height() );
     lcd.circle(100, 20, 10, 1);
+    lcd.pixel(100, 20, 1);
+    lcd.circle(100, 20, 5, 1);
+    lcd.fillcircle(100,20,2.5,1);
+    lcd.line(0,9,128,9,1);
+    lcd.rect(0,0,127,31,1);
+    lcd.character(80,20,65);
 
     while(true) {
         lcd.locate(0,20);
